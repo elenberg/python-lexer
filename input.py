07e39ef8b3e73198eafd30a88d16d12ae3e1dca0
elenberg <everettlenberg@gmail.com>
2016-04-21 12:56:30 -0600

    Initial commit

---
+++ input.py
@@ -0,0 +1,12 @@
+# Comment 1  
+# Comment 2 
+# Factorial: 
+def fact(x):
+   if x == -1:     
+    return 1.i 
+elif x ==0:     
+    return 1   
+else:
+    return x* fact(x - 1) 
+s = "foo\n\'\"" 
+fact(20)
---
+++ lexical_analyzer.py
@@ -0,0 +1,73 @@
+import re
+
+
+class Token():
+
+    keywords = ['and', 'del', 'from', 'not', 'while',
+                'as', 'elif', 'global', 'or', 'with',
+                'assert', 'else', 'if', 'pass', 'yield',
+                'break', 'except', 'import', 'print',
+                'class', 'exec', 'in', 'raise',
+                'continue', 'finally', 'is', 'return',
+                'def', 'for', 'lambda', 'try']
+    id_lex = '[A-Za-z_][A-Za-z_0-9]*'
+
+    delim = ['\(', '\)', '\[', '\]', '\{', '\}', '\-', '\*',
+             '\+', '\/', ':', '\=\=', '\+\=', '\-\=', '\*\=']
+
+    combined_keywords = "(" + "|".join(keywords) + ")"
+    combined_delim = "(" + "|".join(delim) + " )"
+
+    def __init__(self, token):
+        self.tokens = self.find_tokens(token)
+
+    def find_tokens(self, token):
+        return re.split(self.combined_delim, token)
+
+    def get_token_types(self):
+        results = []
+        for token in self.tokens:
+            if self.is_keyword(token):
+                results.append(('KEYWORD', token))
+            elif self.is_lit(token):
+                results.append(('LIT', token))
+            elif self.is_punct(token):
+                results.append(('PUNCT', token))
+            elif self.is_id(token):
+                results.append(('ID', token))
+        return results
+
+    def print_tokens(self):
+        for item in self.get_token_types():
+            print '{} {}'.format(item[0], item[1])
+
+    def is_keyword(self, token):
+        if token in self.keywords:
+            return True
+        return False
+
+    def is_punct(self, token):
+        if re.search('^\W', token) is not None:
+            return True
+        return False
+
+    def is_id(self, token):
+        if re.search(self.id_lex, token) is not None:
+            return True
+        return False
+
+    def is_lit(self, token):
+        if re.search('^[\'\"\d]', token) is not None:
+            return True
+        return False
+
+
+f = open('input.py', 'r')
+for line in f.readlines():
+    individuals = line.split()
+    if '#' in individuals:
+        del individuals[individuals.index('#'):]
+    for i in individuals:
+        token = Token(i)
+        token.print_tokens()
+print "{}".format('ENDMARKER')
